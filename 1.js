bca6190c2ae0fa580f45f31b37c4024ea2ab344c
Le Lam <lamanh28082000@gmail.com>
2020-08-09 21:58:51 +0700

    sua vo theo truc x

--- 1.js
+++ 1.js
@@ -1,4 +1,19 @@
 ﻿ $(function(){
-	 
+	 // hieuung=new TimelineMax({onComplete:noidungvaodi});
+	 // hieuung
+	 // .from($(".trangload"),0.7,{opacity:0})
+	 // .from($(".noidungload"),1,{opacity:0, scale:2})
+	 // .to($(".noidungload"),1,{scale:0.5,ease:Elastic.easOut})
+	 // .to($(".noidungload"),1,{scale:0.2,ease:Elastic.easOut})
+	 // .to($(".noidungload"),1,{scale:1,ease:Elastic.easOut})
+
+
+	 // chuvao=new TimelineMax({paused:true});
+	 // chuvao
+	 // .from($('.textto'),1,{y:0,ease:Back.easeOut,opacity:1});
+	 // // function noidungvaodi()
+	 // // {
+	 // // 	 chuvao.play();
+	 // // }
 })  
  
